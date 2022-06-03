4cba892f6a4d0002efd1cdce8f567004a6761085
Alexander Adkins <alexander.adkins@icloud.com>
2022-06-03 11:38:12 +0200

    nvim-tree udpates

--- config/nvim/lua/user/nvim-tree.lua
+++ config/nvim/lua/user/nvim-tree.lua
@@ -1,106 +1,113 @@
---> Custom Icons
--- stylua: ignore
-vim.g.nvim_tree_icons = {
-  default         = "",
-  symlink         = "",
-  git = {
-    unstaged      = "",
-    staged        = "",
-    unmerged      = "",
-    renamed       = "",
-    deleted       = "",
-    untracked     = "",
-    ignored       = "",
-  },
-  folder = {
-    arrow_open    = "🡳",
-    arrow_closed  = "🡲",
-    default       = "",
-    open          = "",
-    empty         = "⊘",
-    empty_open    = "⊘",
-    symlink       = "",
-  },
-}
-
-vim.g.nvim_tree_symlink_arrow = " ⇆ "
-
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-	return
+  return
 end
 
 local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
 if not config_status_ok then
-	return
+  return
 end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
-nvim_tree.setup({
-	disable_netrw = true,
-	hijack_netrw = true,
-	open_on_setup = false,
-	ignore_ft_on_setup = {
-		"startify",
-		"dashboard",
-		"alpha",
-	},
-	auto_close = true,
-	open_on_tab = false,
-	hijack_cursor = false,
-	update_cwd = true,
-	update_to_buf_dir = {
-		enable = true,
-		auto_open = true,
-	},
-	diagnostics = {
-		enable = true,
-		icons = {
+nvim_tree.setup {
+  hijack_directories = {
+    enable = false,
+  },
+  ignore_ft_on_setup = {
+    "startify",
+    "dashboard",
+    "alpha",
+  },
+  filters = {
+    custom = { ".git" },
+    exclude = { ".gitignore" },
+  },
+  update_cwd = true,
+  renderer = {
+    add_trailing = false,
+    group_empty = false,
+    highlight_git = false,
+    highlight_opened_files = "none",
+    root_folder_modifier = ":t",
+    indent_markers = {
+      enable = false,
+      icons = {
+        corner = "? ",
+        edge = "? ",
+        none = "  ",
+      },
+    },
+    icons = {
+      webdev_colors = true,
+      git_placement = "before",
+      padding = " ",
+      symlink_arrow = " ⇆ ",
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        folder = {
+          arrow_open    = "🡳",
+          arrow_closed  = "🡲",
+          default       = "",
+          open          = "",
+          empty         = "⊘",
+          empty_open    = "⊘",
+          symlink       = "",
+        },
+        git = {
+          unstaged      = "",
+          staged        = "",
+          unmerged      = "",
+          renamed       = "",
+          deleted       = "",
+          untracked     = "",
+          ignored       = "",
+        },
+      },
+    },
+  },
+  diagnostics = {
+    enable = true,
+    icons = {
 			hint = "◌",
 			info = "⊙",
 			warning = "⊘",
 			error = "⊗",
-		},
-	},
-	update_focused_file = {
-		enable = true,
-		update_cwd = true,
-		ignore_list = {},
-	},
-	git = {
-		enable = true,
-		ignore = true,
-		timeout = 500,
-	},
-	view = {
-		width = 30,
-		height = 30,
-		hide_root_folder = false,
-		side = "left",
-		auto_resize = true,
-		mappings = {
-			custom_only = false,
-			list = {
-				{ key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-				{ key = "h", cb = tree_cb("close_node") },
-				{ key = "v", cb = tree_cb("vsplit") },
-			},
-		},
-		number = false,
-		relativenumber = false,
-	},
-	--> #1071 fix: https://github.com/kyazdani42/nvim-tree.lua/issues/1071
-	actions = { open_file = { resize_window = true } },
-	quit_on_open = 0,
-	git_hl = 1,
-	disable_window_picker = 0,
-	root_folder_modifier = ":t",
-	show_icons = {
-		git = 1,
-		folders = 1,
-		files = 0,
-		folder_arrows = 1,
-		tree_width = 30,
-	},
-})
+    },
+  },
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+    ignore_list = {},
+  },
+  git = {
+    enable = true,
+    ignore = true,
+    timeout = 500,
+  },
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = "left",
+    mappings = {
+      custom_only = false,
+      list = {
+        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+        { key = "h", cb = tree_cb "close_node" },
+        { key = "v", cb = tree_cb "vsplit" },
+      },
+    },
+    number = false,
+    relativenumber = false,
+  },
+}
+
+
