03831699bc2cc42df3220876d60f02e0a6572aba
Alexander Adkins <alexander.adkins@icloud.com>
2022-06-07 22:24:35 +0200

    fix phpdoc for M1 Mac issue

--- config/nvim/lua/user/treesitter.lua
+++ config/nvim/lua/user/treesitter.lua
@@ -7,11 +7,11 @@ end
 configs.setup({
   ensure_installed = "all", -- one of "all" or a list of languages	
   sync_install = false,
-	ignore_install = { "" }, --> List of parsers to ignore installing
+  ignore_install = { "phpdoc", "tree-sitter-phpdoc" }, -- List of parsers to ignore installing
 	autopairs = { enable = true },
 	highlight = {
 		enable = true, --> false will disable the whole extension
-		disable = { "" }, --> list of language that will be disabled
+    disable = { "phpdoc", "tree-sitter-phpdoc" }, -- list of language that will be disabled
 		additional_vim_regex_highlighting = true,
 	},
 	indent = { enable = true, disable = { "yaml" } },
